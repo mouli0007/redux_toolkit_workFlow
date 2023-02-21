c720366beb9e6e88d0b95fdf68a621bef57f0faa
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-02-21 20:36:58 +0530

    Update redux_async_thunk_workflow.js

--- redux_async_thunk_workflow.js
+++ redux_async_thunk_workflow.js
@@ -19,3 +19,37 @@ export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
     return err.message;
   }
 });
+
+
+  // Extra Reducers !
+
+  extraReducers(builder) {
+    builder
+      // Pending
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = "loading";
+      })
+      // FullFilled !
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = "Succeeded";
+
+        let min = 1;
+        const loadedPost = action.payload.map((post) => {
+          post.date = sub(new Date(), { minutes: min++ }).toISOString();
+          post.reactions = {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0,
+          };
+          return post;
+        });
+        state.posts = state.posts.concat(loadedPost);
+      })
+      // Rejected !
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = "Failed";
+        state.error = action.error.message;
+      });
+  },
